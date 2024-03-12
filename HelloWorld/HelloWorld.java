fde8e4709a0afca5dd3a5dec8b34990791b8d684
Vinnceros <99991052+vinnceros@users.noreply.github.com>
2024-03-12 11:26:13 -0700

    Add files via upload

---
+++ HelloWorld/HelloWorld.java
@@ -0,0 +1,7 @@
+public class HelloWorld
+{
+    public static void main(String[] args)
+    {
+        System.out.println ("Hello Everyone, I am Vinnceros, and I am relatively new to Java, having only used it for website design previously, but never fully coding it in. This file has been created so I could interact with GitHub Desktop.");
+    }
+}
